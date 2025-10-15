97bf73b4f59f18215b86ee9d86e5d46421edba0b
Dev <devpancholigt2004@gmail.com>
2025-10-16 00:22:10 +0530

    created all the api and and applied migrations on the db and dumps the sql data and created the requirements files and added the images of the product

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_jwt_extended import JWTManager
 from models import db
+from routers.api import api
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -12,8 +13,8 @@ CORS(app)
 JWTManager(app)
 
 db.init_app(app)
-
-
+app.register_blueprint(api, url_prefix='/api')
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     with app.app_context():
