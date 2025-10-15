f1a7a4bbe80d77a7c9a5f85ff8d4c74bcc0cd639
Dev <devpancholigt2004@gmail.com>
2025-10-15 14:12:22 +0530

    config the the connection of mysql db and other settings, in the app.py file linked the mysql connection and jwt manager and created all the tables, and in the models.py file created all the models which are required for the page

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from flask_jwt_extended import JWTManager
+from models import db
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+CORS(app)
+JWTManager(app)
+
+print("DB URL:", app.config["SQLALCHEMY_DATABASE_URI"])
+db.init_app(app)
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,57 @@
+from flask_sqlalchemy import SQLAlchemy
+import passlib
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    manufacturer_name = db.Column(db.String(100), nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, nullable=False, default=0)
+    image_url = db.Column(db.String(255), nullable=True)
+
+    def __repr__(self):
+        return f'<Product id : {self.id} name : {self.name}>'
+
+class Description(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+    product = db.relationship('Product', backref=db.backref('descriptions', lazy=True))
+
+    def __repr__(self):
+        return f'<Description id : {self.id} product_id : {self.product_id}>'
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text, nullable=True)
+
+    product = db.relationship('Product', backref=db.backref('reviews', lazy=True))
+    user = db.relationship('User', backref=db.backref('reviews', lazy=True))
+
+    def __repr__(self):
+        return f'<Review id : {self.id} product_id : {self.product_id} user_id : {self.user_id}>'
+    
+class Salt(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    salt_name = db.Column(db.String(64), nullable=False)
+
+    product = db.relationship('Product', backref=db.backref('salt', uselist=False))
+
+    def __repr__(self):
+        return f'<Salt id : {self.id} product_id : {self.product_id}>'
